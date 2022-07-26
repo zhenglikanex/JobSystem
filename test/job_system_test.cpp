b2fe513da82c24d96716745a17a5d5455a593775
zhenglikanex <413938512@qq.com>
2022-07-26 16:38:14 +0800

    修改

--- test/job_system_test.cpp
+++ test/job_system_test.cpp
@@ -10,18 +10,13 @@ int main()
 {
 	std::atomic_uint32_t jobs = 0;
 
-	int i1 = jobs.fetch_add(1);
-	int i2 = jobs.fetch_sub(1);
-
 	auto& job_system = JobSystem::Get();
 	std::vector<float> vec(500000, 10);
 	job_system.Start(std::thread::hardware_concurrency());
-
 	std::cout << "begin" << std::endl;
 	{
 		Timer t("time");
-		auto group = job_system.CreateJob([](Job*) {});
-		std::function<void(float*, uint32_t)> func = [group](float* data, uint32_t count)
+		std::function<void(float*, uint32_t)> func = [](float* data, uint32_t count)
 		{
 			for (int i = 0; i < count; ++i)
 			{
@@ -36,9 +31,7 @@ int main()
 		auto root = job_system.ParallelFor(vec.data(), vec.size(), func, 256);
 
 		job_system.Run(root);
-		job_system.Run(group);
 		job_system.Wait(root);
-		job_system.Wait(group);
 	}
 
 	std::cout << jobs << std::endl;
